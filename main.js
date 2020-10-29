6242ff04581a6d0db609857e2e9b460cffa0ec3d
bionut <balcau.ionut@gmail.com>
2020-10-29 15:40:11 +0200

    input in iframes electron issue

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow, session } = require('electron')
 const path = require('path')
+const CDP = require('chrome-remote-interface')
+
+
+app.commandLine.appendSwitch('remote-debugging-port', '9222')
 
 let mainWindow
 
@@ -14,7 +18,7 @@ function createWindow() {
       nativeWindowOpen: true,
       nodeIntegration: false,
       nodeIntegrationInSubFrames: true,
-      offscreen: true,
+      offscreen: false, // try with and without OFFSCREEN
       webSecurity: true
     }
   })
@@ -23,37 +27,105 @@ function createWindow() {
   // mainWindow.openDevTools()
   /*
 
-   ignore the first few updates !
+   CDP = chrome devtools protocol
+   SIE = webContents.sendInputEvent
 
-   expected output (what we get with electron 4):
-    DIRTY  7 { x: 254, y: 254, width: 256, height: 256 }
-    DIRTY  8 { x: 254, y: 254, width: 256, height: 256 }
-    DIRTY  9 { x: 254, y: 254, width: 256, height: 256 }
-    DIRTY  10 { x: 254, y: 254, width: 256, height: 256 }
-    DIRTY  11 { x: 254, y: 254, width: 256, height: 256 }
+   Method | Electron version | offscreen  => result
+  --------------------------------------------
+   CDP | 10 | false => PASS
+   CDP | 10 | true => FAIL
+
+   CDP | 5  | false => PASS
+   CDP | 5  | true => FAIL
+
+   CDP | 4  | false => PASS
+   CDP | 4  | true => PASS
+
+   SIE | 10 | false => FAIL
+   SIE | 10 | true => FAIL
+
+   SIE | 5  | false => FAIL
+   SIE | 5  | true => FAIL
 
 
-
-   output with electron >=5.0.0:
-    DIRTY  9 { x: 0, y: 0, width: 800, height: 575 }
-    DIRTY  10 { x: 0, y: 0, width: 800, height: 575 }
-    DIRTY  11 { x: 0, y: 0, width: 800, height: 575 }
-    DIRTY  12 { x: 0, y: 0, width: 800, height: 575 }
-    DIRTY  13 { x: 0, y: 0, width: 800, height: 575 }
-    DIRTY  14 { x: 0, y: 0, width: 800, height: 575 }
+   SIE | 4  | false => PASS
+   SIE | 4  | true => PASS
 
 */
-  mainWindow.loadURL('https://output.jsbin.com/vurotim') // the page is just a div switching between red and blue once every second
 
-  let imgCount = 0;
-  mainWindow.webContents.beginFrameSubscription(true, (nativeImage, dirtyRect) => {
-    console.log('DIRTY ', imgCount++, dirtyRect)
+  let passed = false;
+
+  mainWindow.loadURL('https://output.jsbin.com/higoxaj') // the page includes an iframe (below). the iframe has a 'click' listeners that sets body background green
+  // mainWindow.loadURL('https://crystal-test-site--balcauionut.repl.co/testpages/click-change-background.html');
+
+  mainWindow.webContents.on('console-message', (e, level, message) => {
+    console.log('Console:', message)
+    if (message.includes('CLICKED')) passed = true;
   })
 
+  mainWindow.webContents.on('did-finish-load', async () => {
+
+    await timeout(500);
+    await clickElectronSendInputEvent()
+    // await clickCDP()
+
+    await timeout(300)
+    if (passed) console.log('test PASSED')
+    else console.log('test FAILED')
+
+  })
+
+
+  async function clickCDP() {
+
+      // connect to endpoint
+      let client = await CDP({
+        target: (list) => { // need to return the index of which window we want to debug
+          // it seems like windows are always on index 0, haven't found a different way to match them
+          return list.findIndex(target => !/^devtools:/.test(target.url) && !/^chrome-devtools:/.test(target.url));
+        }
+      });
+
+      let msg = {
+        type: 'mousePressed', // Allowed Values: mousePressed, mouseReleased, mouseMoved, mouseWheel
+        x: 200,
+        y: 200,
+        button: 'left', // Allowed Values: none, left, middle, right, back, forward
+        buttons: 1, // A number indicating which buttons are pressed on the mouse when a mouse event is triggered. Left=1, Right=2, Middle=4, Back=8, Forward=16, None=0.
+        clickCount: 1
+      }
+
+      await client.send('Input.dispatchMouseEvent', msg)
+      msg.type = 'mouseReleased';
+      await client.send('Input.dispatchMouseEvent', msg)
+  }
+
+
+  async function clickElectronSendInputEvent() {
+    let event = {
+      type: 'mouseDown',
+      clickCount: 1,
+      button: 'left',
+      x: 200,
+      y: 200,
+      globalX: 200,
+      globalY: 200,
+      modifiers: ['leftButtonDown']
+    };
+
+    mainWindow.webContents.sendInputEvent(event);
+    await timeout(0);
+    event.type = 'mouseUp'
+    mainWindow.webContents.sendInputEvent(event)
+  }
 
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+}
+
+function timeout(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration))
 }
 
 app.on('ready', createWindow)
